3e1710bf4c6b835c1c4d36a1e6d7fb255451361b
Seng <yangsx7908@uwec.edu>
2020-05-22 17:58:16 +0000

    test

--- weather-serv/app.js
+++ weather-serv/app.js
@@ -11,6 +11,29 @@ let credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
 let connection = mysql.createConnection(credentials);
 connection.connect();
 
+function rowToObject(row){
+	return{
+		city: row.city,
+		state: row.state,
+		country: row.country,
+		weatherCond: row.weatherCond,
+		temp: row.temp,
+		tempFeels: row.tempFeels,
+		id: row.id
+	};
+}
+
+app.get('status/:city/:state', (request, response) => {
+	const query = 'SELECT city, state, country, weatherCond, temp, tempFeels, id FROM weather';
+	const params = [request.params.city, request.params.state];
+	connection.query(query, params, (error, rows) => {
+		response.send({
+			ok: true,
+			status: rows.map(rowToObject),
+		});
+	});
+});
+
 const port = 3444;
 app.listen(port, () => {
 	console.log(`We're live on port ${port}!`);
